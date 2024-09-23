cc544e2c86d60a9fc2e60efdadb0ca7f6c8ec0e5
AkshitaVyas27 <akshitavyas1302@gmail.com>
2024-09-23 15:59:21 +0530

    mcpi asgns

---
+++ assignDay5_1/Src/intr.c
@@ -0,0 +1,27 @@
+/*
+ * intr.c
+ *
+ *  Created on: Sep 22, 2024
+ *      Author: asus
+ */
+
+#include"int.h"
+#include"led.h"
+
+void SwitchInIt(void){
+	RCC->AHB1ENR|=BV(SWITCH_CLOKEN);
+	SWITCH_GPIO->MODER&= ~(BV(SWITCH_PIN*2+1)|BV(SWITCH_PIN*2));
+	SWITCH_GPIO->OTYPER&= ~BV(SWITCH_PIN);
+	SWITCH_GPIO->PUPDR &= ~(BV(SWITCH_PIN*2+1) | BV(SWITCH_PIN*2));
+	EXTI->FTSR |= BV(SWITCH_EXTI);
+	EXTI->IMR |= BV(SWITCH_EXTI);
+	SYSCFG->EXTICR[0]&= ~(BV(3)|BV(2)|BV(1)|BV(0));
+	NVIC_EnableIRQ(EXTI0_IRQn);
+}
+uint32_t SwitchExtiFlag = 0;
+uint32_t count=0;
+
+void EXTI0_IRQHandler(void){
+	EXTI->PR |= BV(SWITCH_EXTI);
+	SwitchExtiFlag=1;
+}
