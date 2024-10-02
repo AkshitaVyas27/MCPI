2f9424d95e213cea201d335f284edccdd141544d
AkshitaVyas27 <akshitavyas1302@gmail.com>
2024-10-02 21:37:36 +0530

    Assignment

---
+++ AssignDay13_2/Inc/int.h
@@ -0,0 +1,22 @@
+/*
+ * int.h
+ *
+ *  Created on: Sep 22, 2024
+ *      Author: asus
+ */
+
+#ifndef INT_H_
+#define INT_H_
+#include "stm32f4xx.h"
+#define SWITCH_GPIO GPIOA
+#define SWITCH_PIN 0
+#define SWITCH_CLOKEN 0
+#define SWITCH_EXTI 0
+
+void SwitchInIt(void);
+void EXTI0_IRQHandler(void);
+
+extern uint32_t SwitchExtiFlag;
+
+
+#endif /* INT_H_ */
